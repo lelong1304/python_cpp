5539648c141d4d4a155846f5916a790f19491691
lelong1304 <lelong1304@gmail.com>
2021-03-07 20:37:12 +0000

    add code

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -10,6 +10,23 @@ char const* greet() {
   return "hello, world";
 }
 
+
+class People {
+  private:
+    std::string firstName;
+    std::string lastName;
+  public:
+    People() {}
+    People(const std::string &firstName, const std::string &lastName) : firstName(firstName), lastName(lastName) {}
+    const std::string &getFirstName() const {
+      return firstName;
+    }
+    const std::string &getLastName() const {
+      return lastName;
+    }
+    void setFirstName(const std::string &firstName_) { firstName = firstName_; }
+    void setLastName(const std::string &lastName_) { lastName = lastName_; }
+};
 namespace py = pybind11;
 
 
@@ -19,4 +36,13 @@ PYBIND11_MODULE(hello_component,greetings)
   
   greetings.def("greet", &greet, "a function saying hello");
   greetings.def("getVersion", &getVersion, "a function returning the version");
+
+  //
+  py::class_<People>(greetings, "People",py::dynamic_attr())
+        .def(py::init<const std::string &, const std::string &>())
+        .def("setFirstName", &People::setFirstName)
+        .def("setLastName", &People::setLastName)
+        .def("getFirstName", &People::getFirstName)
+        .def("getLastName", &People::getLastName);
+ 
 }
